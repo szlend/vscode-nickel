e29c6a1e548fc5518573d90b35fad1b5b5a4ad7c
Simon Žlender <pub.git@zlender.si>
2023-06-17 15:29:56 +0200

    build: bundle extension

--- flake.nix
+++ flake.nix
@@ -10,13 +10,7 @@
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            nodejs
-            vsce
-            nickel
-            nls
-            topiary
-          ];
+          packages = with pkgs; [ nodejs vsce ];
         };
       };
     };
